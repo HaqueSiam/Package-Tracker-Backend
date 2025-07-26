c56f4e049feaa5a03172b7a55f9ddcaa4ccaf80a
HaqueSiam <haquesiam99@gmail.com>
2025-07-26 12:08:11 +0600

    u

--- api/index.js
+++ api/index.js
@@ -1,22 +1,13 @@
-// === File: server/index.js ===
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const packageRoutes = require('../routes/packages');
-//require('dotenv').config();
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+import app from '../index.js';
 
-app.use(cors());
-app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('✅ MongoDB Connected'))
-  .catch((err) => console.error('❌ MongoDB Error:', err));
+let isInitialized = false;
 
-app.use('/api/packages', packageRoutes);
-
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+export default async function handler(req, res) {
+  if (!isInitialized) {
+    
+    isInitialized = true;
+  }
+  app(req, res);
+}

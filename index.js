09ff953071f340c3cb4956dd1e3ab1252d16e0e7
HaqueSiam <haquesiam99@gmail.com>
2025-07-26 12:21:04 +0600

    u

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('✅ MongoDB Connected'))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('✅ MongoDB Connected'))
   .catch((err) => console.error('❌ MongoDB Error:', err));
 
 app.use('/api/packages', packageRoutes);
